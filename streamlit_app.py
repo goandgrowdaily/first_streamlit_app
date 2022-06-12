7b6050f788a69b43abaf51e9f858fb90c2860778
goandgrowdaily <106006506+goandgrowdaily@users.noreply.github.com>
2022-06-12 14:51:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import snowflake.connector
 import pandas
 
-from urlib.error import URLError
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Fevorites')
