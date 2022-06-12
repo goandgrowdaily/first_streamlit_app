82224bf59c3830db80645954ff16a50ba6bb2bb0
goandgrowdaily <106006506+goandgrowdaily@users.noreply.github.com>
2022-06-12 14:31:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,3 +31,4 @@ streamlit.dataframe(my_data_rows)
 
 streamlit.header('What would you like to add')
 add_my_fruit = streamlit.text_input('add the fruit:')
+streamlit.write('Thanks for adding fruit : ',add_my_fruit)
